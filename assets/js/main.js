4f26072f2d9f192d53a5375b6f3f29c9fb3973dc
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-08-22 15:13:21 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato, che va formattato in questo modo: nomecognomecolorepreferito23 
+Esempio: fabiopacificiblue23 */
+
+/* 
+
+prompt per 
+-nome completo
+-colore
+-età
+
+"sommare" i log dei prompt e stampare in console e in html
+
+*/
+
